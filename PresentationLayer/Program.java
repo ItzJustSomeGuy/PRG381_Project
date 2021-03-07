7c66ee83115e5b08998a5bca6ecfca1419de7972
MeyerDC <576142@student.belgiumcampus.ac.za>
2021-03-07 23:50:20 +0200

    final commit

--- PresentationLayer/Program.java
+++ PresentationLayer/Program.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Program {
 
 
-    public static void main(String[] args) 
+    public static void main(String[] args) throws ClassNotFoundException {
         
         
         ProgramMethods pm = new ProgramMethods();
