e1026dca2b3b78da2fd85cc8d04f3c6261884370
ItzJustSomeGuy <sebastiaan.wolfaardt@gmail.com>
2021-03-07 21:58:16 +0200

    Finished DbConnection

--- DataAccessLayer/DBConnection.java
+++ DataAccessLayer/DBConnection.java
@@ -10,33 +10,8 @@ import java.util.List;
 
 import BusinessLogicLayer.Bookings;
 import BusinessLogicLayer.Food;
-import BusinessLogicLayer.Venue;
-import jdk.internal.event.Event;
 
 public class DBConnection {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("com.sqlserver.cj.jdbc.Driver");
-
-        try (Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:3306/CateringDB", "root", "");
-                CallableStatement cstmt = con.prepareCall("{call dbo.spGetFood()}");) {
-            List<Food> foods = new LinkedList<Food>();
-            cstmt.setInt(1, 0);
-            ResultSet rs = cstmt.executeQuery();
-
-            // Iterate through the data in the result set and display it.
-            while (rs.next()) {
-                Food food = new Food(rs.getInt(1), rs.getString(2), rs.getString(4), rs.getDouble(3));
-            }
-
-            for (Food food : foods) {
-                System.out.println(food.getMealChoice());
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
 
     public List<Food> getFood() throws ClassNotFoundException {
         Class.forName("com.sqlserver.cj.jdbc.Driver");
@@ -62,15 +37,13 @@ public class DBConnection {
     public List<Bookings> getEvent() throws ClassNotFoundException {
         Class.forName("com.sqlserver.cj.jdbc.Driver");
         List<Bookings> bookings = new LinkedList<Bookings>();
-        Venue ven;
         try (Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:3306/CateringDB", "root", "");
                 CallableStatement cstmt = con.prepareCall("{call dbo.spGetEve()}");) {
             cstmt.setInt(1, 0);
             ResultSet rs = cstmt.executeQuery();
-
             // Iterate through the data in the result set and display it.
             while (rs.next()) {
-                Bookings booking = new Bookings(ven.equals(rs.getString(7)), rs.getString(2), rs.getString(4), rs.getDouble(3));
+                Bookings booking = new Bookings(rs.getString(2), rs.getFloat(3));
                 bookings.add(booking);
             }
 
@@ -80,19 +53,44 @@ public class DBConnection {
         return bookings;
     }
 
-    public void insertClient(String name, String surname, String phone, String Email) {
+    public void insertClient(String name, String surname, String phone, String Email) throws ClassNotFoundException {
         Class.forName("com.sqlserver.cj.jdbc.Driver");
         try (Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:3306/CateringDB", "root", "");
                 CallableStatement cstmt = con.prepareCall("{call dbo.spInsertClient(?,?,?,?)}");) {
 
-            cstmt.(1, 0);
+            cstmt.setInt(1, 0);
             ResultSet rs = cstmt.executeQuery();
+            rs.insertRow();
 
-            // Iterate through the data in the result set and display it.
-            while (rs.next()) {
-                Food food = new Food(rs.getInt(1), rs.getString(2), rs.getString(4), rs.getDouble(3));
-                foods.add(food);
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertBooking(String type, float price) throws ClassNotFoundException {
+        Class.forName("com.sqlserver.cj.jdbc.Driver");
+        try (Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:3306/CateringDB", "root", "");
+                CallableStatement cstmt = con.prepareCall("{call dbo.spInsertEvent()}");) {
+
+            cstmt.setInt(1, 0);
+            ResultSet rs = cstmt.executeQuery();
+            rs.insertRow();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteEvent(int eventID) throws ClassNotFoundException {
+        Class.forName("com.sqlserver.cj.jdbc.Driver");
+        try (Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost:3306/CateringDB", "root", "");
+                CallableStatement cstmt = con.prepareCall("{call dbo.spDeleteEvent(?)}");) {
+
+            cstmt.setInt(eventID, 0);
+            ResultSet rs = cstmt.executeQuery();
+            rs.insertRow();
+
 
         } catch (SQLException e) {
             e.printStackTrace();
